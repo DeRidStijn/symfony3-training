8c8565ec1dd9949b67a3b0b70a4e0656412fc465
De Ridder Stijn <deridder.stijn93@gmail.com>
2018-03-21 17:24:43 +0100

    voorlopige tussenstand

--- project/src/AppBundle/Controller/BlogController.php
+++ project/src/AppBundle/Controller/BlogController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Blogpost;
+use AppBundle\Entity\Commentpost;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
@@ -30,6 +31,40 @@ class BlogController extends Controller
             'blogposts' => $blogposts,
         ));
     }
+   
+    /**
+     * Finds and displays a blogpost entity.
+     * @Route("/{id}", name="blogpost_show")
+     * @Method({"GET", "POST"})
+     */
+    public function showAction(Blogpost $blogpost)
+    { // create the form here
+        $commentForm!->handleRequest($request);
+        if ($commentForm->isSubmitted() &&$commentForm->isValid()) {
+            $comment = $commentForm->getData();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($comment); 
+            $entityManager->flush();
+            return $this->redirectToRoute('blogpost_show'); 
+        }
+    }
+
+    public function newcommentAction(request $request)
+    {
+        $comment = new Comment();
+        $comment->setBlogpost($blogpost);
+        $commentForm = $this->createFormBuilder($comment) 
+            ->add('name', TextType::class)
+            ->add('comment', TextareaType::class)
+            ->add('save', SubmitType!::class, array('label' =>'Submit comment')) 
+            ->getForm(); 
+        return $this->render('blogpost/show.html.twig', array( 
+            'blogpost' => $blogpost,    
+            'comments' => $comments,     
+            'delete_form' => $deleteForm->createView(), 
+            'comment_form' =>$commentForm->createView(), ));
+    }
+
 
     /**
      * Finds and displays a blogpost entity.
@@ -41,7 +76,7 @@ class BlogController extends Controller
     {
 
         return $this->render('blog/show.html.twig', array(
-            'blogpost' => $blogpost
+            'blogposts' => $blogpost
         ));
     }
 }
